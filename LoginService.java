2348f92085863bd3d8419fecd6c99662efd8844d
Yunchun <yup086@ucsd.edu>
2021-11-07 11:39:27 -0600

    add sign service file

---
+++ LoginService.java
@@ -0,0 +1,22 @@
+package com.example.sportgather.service;
+
+import com.example.sportgather.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class LoginService {
+
+    @Autowired
+    private static UserRepository userRepository;
+
+    public LoginService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public static String showId(String email){
+        String id = userRepository.displayId(email);
+        return id;
+    }
+
+}
