6ea21c3ee842ac695479e37462dc2dd65e1d2345
Yunchun <yup086@ucsd.edu>
2021-11-09 14:24:33 -0600

    updated controller files

--- backend/sportgather/src/main/java/com/example/sportgather/controller/LoginController.java
+++ backend/sportgather/src/main/java/com/example/sportgather/controller/LoginController.java
@@ -1,11 +1,10 @@
 package com.example.sportgather.controller;
 
-
 import com.example.sportgather.service.LoginService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
+import javax.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping(path = "login")
@@ -13,23 +12,22 @@ public class LoginController {
     private final LoginService loginService;
 
     @Autowired
-
     public LoginController(LoginService loginService) {
         this.loginService = loginService;
     }
 
-    @PostMapping(path="/saveUser/{firstName}/{email}/{password}")
-    public String saveUser(@PathVariable("firstName") String firstName,
-                           @PathVariable("email") String email,
-                           @PathVariable("password") String password) {
-        System.out.println("saveUser is called");
-        return LoginService.addUser(firstName, email, password);
-    }
-
-    @GetMapping(path = "/{email}")
-    public String displayId(@PathVariable("email") String email) {
-        System.out.println("displayId is called");
-        return LoginService.showId(email);
+    @GetMapping(path = "/{email}/{password}")
+    public String displayId(@PathVariable("email") String email, @PathVariable("password") String password, HttpSession session) {
+        String id = LoginService.showId(email);
+        String pass = LoginService.showPassword(email);
+        if (id != "" & pass.equals(password)) {
+            session.setAttribute("userid", id);
+            System.out.println("displayId is called");
+            return id;
+        }
+        else {
+            return "email or password is wrong";
+        }
     }
 
 }
--- backend/sportgather/src/main/java/com/example/sportgather/controller/ProfileController.java
+++ backend/sportgather/src/main/java/com/example/sportgather/controller/ProfileController.java
@@ -4,10 +4,13 @@ import com.example.sportgather.domain.User;
 import com.example.sportgather.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
 @RequestMapping(path = "profile")
+
 public class ProfileController {
 
     private final ProfileService profileService;
@@ -26,14 +29,17 @@ public class ProfileController {
 
     @GetMapping(path = "/{id}")
     public List<User> findUserById(@PathVariable("id") String id) {
+        // String id = (String) session.getAttribute("userid");
+        // System.out.println(id);
         System.out.println("findUserById is called");
         return ProfileService.queryUserById(id);
     }
 
+    @CrossOrigin
     @PutMapping(path="/updateInfo/{lastName}/{gender}/{age}/{phone}/{location}/{type}/{id}")
     public String updateInfo(@PathVariable("lastName") String lastName,
-                             @PathVariable("gender") String gender,
-                             @PathVariable("age") Integer age,
+                           @PathVariable("gender") String gender,
+                           @PathVariable("age") Integer age,
                              @PathVariable("phone") String phone,
                              @PathVariable("location") String location,
                              @PathVariable("type") String type,
@@ -41,5 +47,7 @@ public class ProfileController {
         System.out.println("updateInfo is called");
         return ProfileService.modifyInfo(lastName, gender, age, phone, location, type, id);
     }
+
+
 
 }
