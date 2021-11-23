08b15d3e798c156500ec8795c02d6a5165b0464c
Yunchun <yup086@ucsd.edu>
2021-11-22 21:42:52 -0600

    updated profile page

--- frontend/material-tailwind-kit-react-main/src/pages/Profile.js
+++ frontend/material-tailwind-kit-react-main/src/pages/Profile.js
@@ -1,12 +1,14 @@
+import GatherSportNav from 'components/GatherSportNav.js';
 import Header from 'components/profile/Header';
 import Content from 'components/profile/Content';
 
 export default function Profile() {
     return (
         <>
+            <GatherSportNav username="Ruth Sabin"/>
             <main>
                 <Header />
-               <Content />
+                <Content />
             </main>
         </>
     );
